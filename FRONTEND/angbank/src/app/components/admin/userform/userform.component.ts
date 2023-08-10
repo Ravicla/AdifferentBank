20526f3bb1abf42978913294a55eeb56f56c17d4
Thalia <ravicla1993@gmail.com>
2023-08-10 17:22:39 -0500

    validaciones de formulario

--- FRONTEND/angbank/src/app/components/admin/userform/userform.component.ts
+++ FRONTEND/angbank/src/app/components/admin/userform/userform.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
   styleUrls: ['./userform.component.css']
 })
-export class UserformComponent {
+export class UserformComponent implements OnInit{
+
+  formModel: FormGroup
+  constructor(){
+    this.formModel= new FormGroup({
+      //los datos se cogen del formControlName 
+      identificationNumber: new FormControl('', [
+        Validators.required
+      ]),
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      lastName: new FormControl('', []),
+      birthdate: new FormControl('', []),
+      emailAddress: new FormControl('', []),
+      phoneNumber: new FormControl('', []),
+      rol: new FormControl('', [])
+
+    }, [])
+  }
+
+  ngOnInit(): void {
+  }
+
+  getDataForm(){
+    if (this.formModel.valid) {
+      console.log(this.formModel.value)
+    } else {
+      alert('el formulario no esta bien relleno')
+    }
+  }
+
+  checkControl(pControlName: string, pError: string): boolean {
+    if (this.formModel.get(pControlName)?.hasError(pError) && this.formModel.get(pControlName)?.touched) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
